3221ae1aaaeb95ba0bb17ccf0aa31379b105f821
Hien <dkrykovwork@gmail.com>
2019-03-10 09:43:17 -0700

    modified:   FahrsToCelsius_For.c

--- fahrAndCelsius/FahrsToCelsius_For.c
+++ fahrAndCelsius/FahrsToCelsius_For.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
+#define STEP 20
+#define MIN_TEMP 0
+#define MAX_TEMP 300
+
 int main(int argc, char const *argv[])
 {
 	float celsius, fahr;
-	int step, min_temp, max_temp;
-
-	step = 20;
-	min_temp = 0;
-	max_temp = 300;
-	celsius = 0;
 
 	printf("%s\t%s\n", "Fahrenheits", "Celsius");
 
-	for (fahr = max_temp; fahr >= min_temp; fahr -= step){
-		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		printf("%11.f%12.1f\n", fahr, celsius);
+	for (fahr = MAX_TEMP; fahr >= MIN_TEMP; fahr -= STEP){
+		printf("%11.f%12.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 	}
 
 	return 0;
