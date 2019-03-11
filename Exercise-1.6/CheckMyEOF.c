8550ff3fcecb213ae755625d723f977eb6830304
Hien <dkrykovwork@gmail.com>
2019-03-11 15:27:37 -0700

    Reworked tasks structure

---
+++ Exercise-1.6/CheckMyEOF.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Please, input a character\n");
+	printf("%d\n", getchar() != EOF);
+
+	return 0;
+}
---
+++ Exercise-1.8/CountSymbols.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int c;
+	double space,tab,ns;
+
+	printf("\nPress any key...\n");
+	printf("*****************************\n");
+	printf("Press Ctrl-D for end of input\n\n");
+
+
+	while((c = getchar()) != EOF){
+		if (c == ' ')
+			++space;
+		if (c == '\t')
+			++tab;
+		if (c == '\n')
+			++ns;
+	}
+
+	printf("Number of spaces: %.0f\n", space);
+	printf("Number of tabs: %.0f\n", tab);
+	printf("Number of strings: %.0f\n", ns);
+
+	return 0;
+}
