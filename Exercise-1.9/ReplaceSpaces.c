d0bfbd62cdf58f45c00ac9df13356adea3c28ee3
Hien <dkrykovwork@gmail.com>
2019-03-14 05:50:49 -0700

    Added Exercise 1.9

---
+++ Exercise-1.9/ReplaceSpaces.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int c, isSpace;
+	isSpace = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			if (!isSpace) {
+				putchar(c);
+				isSpace = 1;
+			}
+		} else {
+			putchar(c);
+			isSpace = 0;
+		}
+	}
+
+	return 0;
+}
