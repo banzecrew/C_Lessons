06596075bb56a2d5690a9f33f2b22b75625f11a6
dkrykovwork <dkrykovwork@gmail.com>
2019-03-19 18:39:54 +0300

    modified:   LenToBarChart.c

--- Exercise-1.13/LenToBarChart.c
+++ Exercise-1.13/LenToBarChart.c
@@ -6,35 +6,85 @@
 
 int main(int argc, char const *argv[])
 {
-	int c, i, state;
+	int c, i, elems, word_len, state;
 	int words[CAPACITY];
 
 	state = OUT_WORD;
+	elems = word_len = 0;
 
 	for (i = 0; i < CAPACITY; ++i)
 		words[i] = 0;
 
-	i = 0;
-
-	while ((c = getchar()) != EOF && i < CAPACITY) {
+	while ((c = getchar()) != EOF && elems < CAPACITY) {
 		if (c == ' ' || c == '\n' || c == '\t') {
 			if (state == IN_WORD) {
 				state = OUT_WORD;
-				words[i] = ' ';
-				++i;
+				words[elems] = ' ';
+				++elems;
 			}
 		}
-		else {
+		else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
 			state = IN_WORD;
-			words[i] = c;
-			++i;
+			words[elems] = c;
+			++elems;
 		}
 	}
 
-	for (i = 0; i <= CAPACITY; ++i)
+	if (elems == CAPACITY)
+		printf("Word count limit reached\n");
+
+	printf("Your words: \n");
+
+	for (i = 0; i < elems; ++i)
 		printf("Words[%d]: %7c\n", i, words[i]);
 
-	
+	for (i = 0; i < elems; ++i) {
+
+	}
 
 	return 0;
 }
+
+/*
+
+		variant 1
+		_________________________
+	   /________________________/|
+	+8|							||
+	+7|							||
+	+6|							||
+	+5|							||
+	+4|			---				||
+	+3| ---     ---				||
+	+2| ---     ---       ---	||
+	+1| ---		---		  ---	||
+	  |_________________________|/
+	    hey		test	  ne
+
+
+
+	    variant 2
+
+	+8|
+	+7|
+	+6|
+	+5| <--bidlo
+	+4| <--test chort 
+	+3|	<--hey her lox
+	+2| <--ne da
+	+1|
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
