5e4b6402de260cbacb4e986a0db2a18ac5b40d74
dkrykovwork <dkrykovwork@gmail.com>
2019-03-19 13:42:57 +0300

    fixed Exercise 1-13

--- Exercise-1.13/LenToBarChart.c
+++ Exercise-1.13/LenToBarChart.c
@@ -2,7 +2,7 @@
 
 #define IN_WORD  1
 #define OUT_WORD 0
-#define CAPACITY 50
+#define CAPACITY 10
 
 int main(int argc, char const *argv[])
 {
@@ -11,26 +11,30 @@ int main(int argc, char const *argv[])
 
 	state = OUT_WORD;
 
-	for (i = 0; i <= CAPACITY; ++i)
+	for (i = 0; i < CAPACITY; ++i)
 		words[i] = 0;
 
 	i = 0;
 
-	while ((c = getchar()) != EOF && i <= CAPACITY) {
+	while ((c = getchar()) != EOF && i < CAPACITY) {
 		if (c == ' ' || c == '\n' || c == '\t') {
 			if (state == IN_WORD) {
 				state = OUT_WORD;
+				words[i] = ' ';
 				++i;
 			}
 		}
 		else {
+			state = IN_WORD;
 			words[i] = c;
 			++i;
 		}
 	}
-/*
+
 	for (i = 0; i <= CAPACITY; ++i)
 		printf("Words[%d]: %7c\n", i, words[i]);
-*/
+
+	
+
 	return 0;
 }
