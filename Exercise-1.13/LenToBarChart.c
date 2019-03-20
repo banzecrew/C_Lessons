6e5c638e68f035bc8de0e4406e3e9782eae6d429
dkrykovwork <dkrykovwork@gmail.com>
2019-03-20 18:01:57 +0300

    Fixed word length counter

--- Exercise-1.13/LenToBarChart.c
+++ Exercise-1.13/LenToBarChart.c
@@ -6,13 +6,13 @@
 
 int main(int argc, char const *argv[])
 {
-	int c, i, elems, state;
-	int words[CAPACITY];
+	int c, i, j, elems, state;
+	int words[CAPACITY + 1];
 
 	state = OUT_WORD;
 	elems = 0;
 
-	for (i = 0; i < CAPACITY; ++i)
+	for (i = 0; i <= CAPACITY; ++i)
 		words[i] = 0;
 
 	printf("Enter characters or press \"Ctrl+D\"\n");
@@ -29,11 +29,8 @@ int main(int argc, char const *argv[])
 		}
 	}
 
-	for (i = 0; i < elems; ++i)
-		printf("Words[%d]: %7c\n", i, words[i]);
-
-	if (elems == CAPACITY)
-		printf("Word count limit reached\n");
+	for (i = 0; i <= elems; ++i)
+		printf("Words[%d]: %7c %3d\n", i, words[i], words[i]);
 
 	printf("Dictionary:\n");
 	for (i = 0; i < elems; ++i) {
@@ -46,8 +43,8 @@ int main(int argc, char const *argv[])
 	int     _word_len = 0;
 	int _max_word_len = 0;
 
-	for (i = 0; i < elems; ++i) {
-		if (words[i] != ' ') {
+	for (i = 0; i <= elems; ++i) {
+		if (words[i] != ' ' && words[i] != 0) {
 			++_word_len;
 		} else {
 			if (_word_len > _max_word_len)
@@ -56,30 +53,87 @@ int main(int argc, char const *argv[])
 		}
 	}
 
+	_word_len = 0;
+
 
 	printf("Max word len: %d\n", _max_word_len);
+
+	for (i = 0; i <= (_max_word_len); ++i)
+		printf("__");
+
+	printf("\n\n");
+
+	for (i = 0; i <= elems; ++i) {
+		if (words[i] != ' ' && words[i] != 0) {
+			printf("%c", words[i]);
+			++_word_len;
+		} else {
+			printf("\n");
+			for (j = 0; j < _word_len; ++j)
+				printf("| ");
+			_word_len = 0;
+			printf("\n");
+		}
+	}
+
+	for (i = 0; i <= (_max_word_len); ++i)
+		printf("__");
+
+	printf("\n");
 	
+	for (i = 0; i <= _max_word_len; ++i)
+		printf("%d ", i);
+
+	printf("\n");
+
+	for (i = 0; i <= _max_word_len; ++i)
+		printf("+ ");
+	
+	printf("\n");
+
+
+
+
+	if (elems == CAPACITY)
+		printf("Word count limit reached\n");
 
 
 	return 0;
 }
 
 /*
+	horzontal hystogram
+	************************
+    ########################
+	bidlo### --------*------
+	######## ---------------
+	lox##### ----*----------
+	######## ---------------
+	sobaka # ----------*----
+	######## --------------- 
+	y####### *--------------
+	#########1#2#3#4#5#6#7#8
+
+	
+
+
+
 
 		variant 1
-	    _________________________
- 	   /________________________/|
-	+8|                         ||
-	+7|                         ||
-	+6|                         ||
-	+5|                         ||
-	+4|         ---             ||
-	+3| ---     ---             ||
-	+2| ---     ---       ---   ||
-	+1| ---     ---       ---   ||
-	  |_________________________|/
-	    hey		test	  ne
-
+	
+	_____________
+   
+	bidlo
+	| | | | | 
+	lox
+	| | |
+	chort
+	| | | | |
+	sobaka
+	| | | | | |
+	_____________
+	1 2 3 4 5 6 7
+	+ + + + + + +
 
 
 		variant 2
@@ -106,22 +160,20 @@ int main(int argc, char const *argv[])
 	+8|
 	
 
-
 		variant 4
-	
-	_____________
+	    _________________________
+ 	   /________________________/|
+	+8|                         ||
+	+7|                         ||
+	+6|                         ||
+	+5|                         ||
+	+4|         ---             ||
+	+3| ---     ---             ||
+	+2| ---     ---       ---   ||
+	+1| ---     ---       ---   ||
+	  |_________________________|/
+	    hey		test	  ne
 
-	bidlo
-	| | | | | 
-	lox
-	| | |
-	chort
-	| | | | |
-	sobaka
-	| | | | | |
-	_____________
-	1-2-3-4-5-6-7
-	+ + + + + + +
 
 
 
