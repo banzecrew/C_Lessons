24724d595fb058405a9248d82b3e92708a21a578
Hien <dkrykovwork@gmail.com>
2019-03-21 15:16:34 -0700

    new file:   Exercise-1.14/CharHisto.c

---
+++ Exercise-1.14/CharHisto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_LEN 10
+
+int main(int argc, char const *argv[])
+{
+	int c;
+	int i;
+	int symbols[MAX_LEN + 1];
+
+	for (i = 0; i <= MAX_LEN; ++i)
+		symbols[i] = 0;
+
+	while ((c = getchar()) != EOF) {
+		
+	}
+
+	return 0;
+}
