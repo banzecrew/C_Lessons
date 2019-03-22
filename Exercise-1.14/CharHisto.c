9d7285d4b48a8e761503b217fdb3976409b2157d
dkrykovwork <dkrykovwork@gmail.com>
2019-03-22 14:38:21 +0300

    modified:   Exercise-1.14/CharHisto.c

--- Exercise-1.14/CharHisto.c
+++ Exercise-1.14/CharHisto.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-#define ASCII 255
+#define ASCII      255
+#define SPACE      32
+#define NEW_STRING 10
+#define TABULATION 9
 
 int main(int argc, char const *argv[])
 {
 	int c;
-	int i;
+	int i, j;
 	int symbols[ASCII];
 
 	for (i = 0; i < ASCII; ++i)
@@ -19,11 +22,45 @@ int main(int argc, char const *argv[])
 	for (i = 0; i < ASCII; ++i) {
 		if (i % 4 == 0)
 			putchar('\n');
-		printf("symbols[%d] = %d ", i, symbols[i]);
+		printf("symbols[%c] = %d ", i, symbols[i]);
 	}
 
 	putchar('\n');
 
+	for (i = 0; i < ASCII; ++i) {
+		if (symbols[i] > 0) {
+
+			if (i == SPACE) {
+				printf("Space: ");
+				for (j = 0; j < symbols[i]; ++j) 
+					printf("*");
+				putchar('\n');
+			}
+
+			else if (i == NEW_STRING) {
+				printf("\\n   : ");
+				for (j = 0; j < symbols[i]; ++j)
+					printf("*");
+				putchar('\n');
+			}
+
+			else if (i == TABULATION) {
+				printf("\\t   : ");
+				for (j = 0; j < symbols[i]; ++j)
+					printf("*");
+				putchar('\n');
+			}
+
+			else {
+				printf("%c    : ", i);
+				for (j = 0; j < symbols[i]; ++j)
+					printf("*");
+				putchar('\n');
+			}
+		}
+	}
+
+	putchar('\n');
 
 	return 0;
 }
