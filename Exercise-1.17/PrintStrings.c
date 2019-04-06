c73d68afed5898dcef0faae6770baf14f59a655b
Hien <dkrykovwork@gmail.com>
2019-04-06 04:26:32 -0700

    new file:   Exercise-1.17/PrintStrings.c

---
+++ Exercise-1.17/PrintStrings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define LIP 80
+#define LIMIT 1000
+
+
+int getstrlen(char line[], int lim, int step);
+
+int main(int argc, char const *argv[])
+{
+	long c;
+	char line[LIMIT];
+
+	while ((c = getstrlen(line, LIMIT, LIP)))
+		if (c > LIP)
+			printf("%s\n", line);
+
+	return 0;
+}
+
+int getstrlen(char line[], int lim, int step)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n';) {
+		line[i++] = c;
+	}
+	if (c == '\n')
+		i++;
+
+	line[i] = '\0';
+
+	return i;
+}
