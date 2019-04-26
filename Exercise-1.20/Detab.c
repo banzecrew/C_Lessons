653bf55846133142886abd61acd39167e0116548
Hien <dkrykovwork@gmail.com>
2019-04-26 15:27:34 -0700

    modified:   Exercise-1.20/Detab.c

--- Exercise-1.20/Detab.c
+++ Exercise-1.20/Detab.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
-#define MAXLEN 15
-#define TAB_SIZE 3
+#define MAXLEN   1000
+#define TAB_STOP 8
 
 int getline(char line[], int lim);
 void detab(char line[], int stops);
 
 int main(int argc, char const *argv[])
 {
-	char str[MAXLEN];
+		
+	char line[MAXLEN];
+		
+	while (getline(line, MAXLEN))
+		detab(line, TAB_STOP);
+/*
+	int c, nb, pos;
 
-	while (getline(str, MAXLEN))
-		detab(str, TAB_SIZE);
+	nb = 0;
+	pos = 1;
 
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			nb = TAB_STOP - (pos -1) % TAB_STOP;
+			while (nb > 0) {
+				putchar(' ');
+				++pos;
+				--nb;
+			}
+		} else if (c == '\n') {
+			putchar(c);
+			++pos;
+		} else {
+			putchar(c);
+			++pos;
+		}
+	}
+*/
 	return 0;
 }
 
@@ -34,11 +57,17 @@ int getline(char line[], int lim)
 void detab(char line[], int stops)
 {
 	int i, j;
+	int blanks;
 
-	for (i = 0; line[i] != '\0'; ++i) {
+	blanks = 0;
+
+	for (i = 0, j = 0; line[i] != '\0'; ++i, ++j) {
 		if (line[i] == '\t') {
-			for (j = 0; j < stops; ++j)
+			blanks = stops - j % stops;
+			while (blanks > 0) {
 				putchar(' ');
+				--blanks;
+			} j = -1;
 		} else {
 			printf("%c", line[i]);
 		}
