38e3b284ba2abce6a4e7dccb3168b8be32689f52
hex <dkrykovwork@wavecom.io>
2019-09-30 23:20:30 +0300

    replaced type of array in Reverse.c

--- Exercise-1.19/Reverse.c
+++ Exercise-1.19/Reverse.c
@@ -2,35 +2,44 @@
 
 #define MAXLEN 15
 
-<<<<<<< Updated upstream
-int getline(int line[], int lim);
-void reverse(int line[]);
-int getlen(int line[]);
-=======
-int getline(char str[], int lim);
-void reverse(char str[]);
->>>>>>> Stashed changes
+int getline(char line[], int lim);
+void reverse(char line[]);
+int getlen(char line[]);
 
 
 int main(int argc, char const *argv[])
 {
-	int c;
-	int str[MAXLEN];
+	char c;
+	char str[MAXLEN];
 
+	str[0] = '1';
+	str[1] = '2';
+	str[2] = '3';
+	str[3] = '4';
+	str[4] = '5';
+	str[5] = '\0';
+
+	printf("%s\n", str);
+
+	reverse(str);
+
+	printf("%s\n", str);
+	/*
 	printf("Enter string or press Ctrl+C\n");
 
 	while ((c = getline(str, MAXLEN))) {
-		printf("Original string: %ls\n", str);
+		printf("Original string: %s\n", str);
 		reverse(str);
-		printf("Reversed string: %ls\n", str);
+		printf("Reversed string: %s\n", str);
 	}
-
+	*/
 	return 0;
 }
 
-int getline(int line[], int lim)
+int getline(char line[], int lim)
 {
-	int c, i;
+	int i;
+	char c;
 
 	for (i = 0; i <= lim; ++i)
 		line[i] = 0;
@@ -43,13 +52,14 @@ int getline(int line[], int lim)
 	return i;
 }
 
-void reverse(int line[])
+void reverse(char line[])
 {
 	int begin, len;
 
 	len = getlen(line);
 
-<<<<<<< Updated upstream
+	printf("len is %d\n", len);
+
 	for (begin = 0; begin < (len - begin); ++begin) {
 		line[begin] = line[begin] + line[len];
 		line[len]   = line[begin] - line[len];
@@ -57,19 +67,12 @@ void reverse(int line[])
 	}
 }
 
-int getlen(int line[])
+int getlen(char line[])
 {
 	int i;
 
 	for (i = 0; line[i] != '\0'; ++i);
+	
 
 	return i - 1;
-=======
-	return i;
-}
-
-void reverse(char str[], int len)
-{
-	
->>>>>>> Stashed changes
 }
