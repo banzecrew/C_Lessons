3ee16eaa0367f68b50feac91048ca90aec005b8d
hex <dkrykovwork@wavecom.io>
2019-10-04 23:42:06 +0300

    none

--- Exercise-1.22/transfer.c
+++ Exercise-1.22/transfer.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 
 #define SBREAKER 10
+#define ARRAY_SIZE 30
 
-
-void transfer(int sep);
+void fillArray(char s[], int len);
+void transfer(char ch, int sep);
 
 int main(int argc, char const *argv[])
 {
-    transfer(SBREAKER);
+    int i;
+
+    char sarray[ARRAY_SIZE];
+
+    fillArray(sarray, ARRAY_SIZE);
+
+
+    for (i = 0; sarray[i] != '\0'; ++i)
+        transfer(sarray[i], SBREAKER);
 
     return 0;
 }
 
-void transfer(int sep)
+void fillArray(char s[], int len)
 {
-    int i, ch;
+    int i;
 
-    for (i = 1; (ch = getchar()) != EOF; ++i) {
-        if (i == sep) {
+    for (i = 0; i < (len - 1); ++i)
+        s[i] = 'g';
+    s[i] = '\0';
+}
+
+void transfer(char ch, int sep)
+{
+    int i;   
+    
+    for (i = 1; ch != EOF; ++i) { /* lol */
+        if (sep == i) {
             putchar('\n');
             i = 1;
         }
-        putchar(ch);
+        printf("%c", ch);
     }
 }
 
