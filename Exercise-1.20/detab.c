1d4eaa322fe8d042afec4646a36127a3c82c5ea6
mac-hex <dkrykovwork@wavecom.io>
2019-12-25 03:06:07 +0300

    another one independ test

--- Exercise-1.20/detab.c
+++ Exercise-1.20/detab.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAXLEN   1000
+#define MAXLEN 1000
 #define TAB_STOP 8
 
 int getline(char line[], int lim);
@@ -8,48 +8,53 @@ void detab(char line[], int stops);
 
 int main(int argc, char const *argv[])
 {
-		
-	char line[MAXLEN];
-		
-	while (getline(line, MAXLEN))
-		detab(line, TAB_STOP);
 
-	return 0;
+  char line[MAXLEN];
+
+  while (getline(line, MAXLEN))
+    detab(line, TAB_STOP);
+
+  return 0;
 }
 
 int getline(char line[], int lim)
 {
-	int i, c;
-	
-	for (i = 0; i < lim; ++i)
-		line[i] = '0';
+  int i, c;
 
-	for (i = 0; i < (lim - 1) && (c = getchar()) != '\n'; ++i)
-		line[i] = c;
+  for (i = 0; i < lim; ++i)
+    line[i] = '0';
 
-	line[i] = '\0';
+  for (i = 0; i < (lim - 1) && (c = getchar()) != '\n'; ++i)
+    line[i] = c;
 
-	return i;
+  line[i] = '\0';
+
+  return i;
 }
 
 void detab(char line[], int stops)
 {
-	int i, j;
-	int blanks;
+  int i, j;
+  int blanks;
 
-	blanks = 0;
+  blanks = 0;
 
-	for (i = 0, j = 0; line[i] != '\0'; ++i, ++j) {
-		if (line[i] == '\t') {
-			blanks = stops - (j % stops);
-			while (blanks > 0) {
-				putchar(' ');
-				--blanks;
-			} j = -1;
-		}
-		else {
-			printf("%c", line[i]);
-		}
-	}
-	putchar('\n');
+  for (i = 0, j = 0; line[i] != '\0'; ++i, ++j)
+  {
+    if (line[i] == '\t')
+    {
+      blanks = stops - (j % stops);
+      while (blanks > 0)
+      {
+        putchar(' ');
+        --blanks;
+      }
+      j = -1;
+    }
+    else
+    {
+      printf("%c", line[i]);
+    }
+  }
+  putchar('\n');
 }
--- Exercise-1.21/entab.c
+++ Exercise-1.21/entab.c
@@ -2,43 +2,47 @@
 
 #define TAB_STOP 8
 
-
 void entab(int stop);
 
 int main(int argc, char const *argv[])
 {
-	entab(TAB_STOP);
+  entab(TAB_STOP);
 
-    return 0;
+  return 0;
 }
 
 void entab(int stop)
 {
-	int c, nb, nt, pos;
+  int c, nb, nt, pos;
 
-	nb = nt = 0;
+  nb = nt = 0;
 
   for (pos = 1; (c = getchar()) != EOF; ++pos)
-    if (c == ' ') {
+    if (c == ' ')
+    {
       if (pos % TAB_STOP)
         ++nb;
-      else {
+      else
+      {
         nb = 0;
         ++nt;
       }
-    } else {
+    }
+    else
+    {
       for (; nt > 0; --nt)
-				putchar('\t');
-			if (c == '\t')
-				nb = 0;
-			else  {
-				for (; nb > 0; --nb)
-					putchar('_');
-				putchar(c);
-				if (c == '\n')
-					pos = 0;
-				else if (c == '\t')
-					pos = pos + (TAB_STOP - (pos - 1) % TAB_STOP) - 1;
-			}
-		  }
+        putchar('\t');
+      if (c == '\t')
+        nb = 0;
+      else
+      {
+        for (; nb > 0; --nb)
+          putchar('_');
+        putchar(c);
+        if (c == '\n')
+          pos = 0;
+        else if (c == '\t')
+          pos = pos + (TAB_STOP - (pos - 1) % TAB_STOP) - 1;
+      }
+    }
 }
